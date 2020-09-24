8166d5c490ddc645ad828966722450245f7987f2
Gómez Roque <32652492+groque0720@users.noreply.github.com>
2020-09-24 11:08:24 -0300

    R240920201108

--- routes/web.php
+++ routes/web.php
@@ -42,7 +42,7 @@ use App\User;
 // 	mkdir("storage/"."omar", 0700);
 // });
 
-Route::group(['domain' => '{account}.negocios.test'], function () {
+Route::group(['domain' => '{account}.vidriera.online'], function () {
 	Route::get('/{'.$account.'}','Negocio\NegocioController@public_index');
 	//Route::get('/', 'Negocio\NegocioController@public_index');
     // Route::get('/', function ($account) {
