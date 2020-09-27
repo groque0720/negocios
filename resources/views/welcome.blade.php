3235815b3e9c40df9012b975401f0b6e29290dfa
Gómez Roque <32652492+groque0720@users.noreply.github.com>
2020-09-27 13:27:54 -0300

    R270920201327

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,12 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <meta id="token" name="csrf-token" content="{{ csrf_token() }}">
 
+<meta property="og:locale" content="es_ES" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="Neveras inteligentes para hostelería" />
+<meta property="og:description" content="Las bebidas más frías del mundo" />
+<meta property="og:url" content="https://vidriera.online/" />
+<meta property="og:site_name" content="Wondercool | Neveras inteligentes" />
 <!-- Para whatsapp -->
 <meta property="og:image" content="https://vidriera.online/storage/ideas-en-laser/da2251e6f6dfcb3dca7f4c38265b3e3d.jpg" />
 <meta property="og:image:secure_url" content="https://vidriera.online/storage/ideas-en-laser/da2251e6f6dfcb3dca7f4c38265b3e3d.jpg" />
