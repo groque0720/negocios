9f4b4d97ffb651200e03e4bc6fb5277697ea04ed
Gómez Roque <32652492+groque0720@users.noreply.github.com>
2020-09-28 15:55:24 -0300

    Update index.blade.php

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -3,29 +3,6 @@
 @section('title', $negocio->nombre)
 
 @section('metas');
-	{{-- <title>your keyword rich title of the website and/or webpage</title> --}}
-	<meta property="og:image" content="https://vidriera.online/storage/{{$negocio->logo}}" />
-	<meta property="og:image:secure_url" content="https://vidriera.online/storage/{{$negocio->logo}}" />
-	<!-- MS, fb & Whatsapp -->
-	<!-- MS Tile - for Microsoft apps-->
-	<meta name="msapplication-TileImage" content="https://vidriera.online/storage/{{$negocio->logo}}">
-	<!-- fb & Whatsapp -->
-	<!-- Site Name, Title, and Description to be displayed -->
-	<meta property="og:site_name" content="{{ $negocio->nombre }}">
-	<meta property="og:title" content="{{ $negocio->nombre }}">
-	<meta property="og:description" content="{{ $negocio->descripcion }}">
-	<!-- Image to display -->
-	<meta property="og:image" content="https://vidriera.online/storage/{{$negocio->logo}}">
-	<!-- No need to change anything here -->
-	<meta property="og:type" content="website" />
-	<meta property="og:image:type" content="image/jpeg">
-	<!-- Size of image. Any size up to 300. Anything above 300px will not work in WhatsApp -->
-	<meta property="og:image:width" content="300">
-	<meta property="og:image:height" content="300">
-	<!-- Website to visit when clicked in fb or WhatsApp-->
-	<meta property="og:url" content="https://vidriera.online/{{ $negocio->url }}">
-
-
 
 
 <meta property="keywords" content="recitales por streaming,+Vivo,recitales,streaming,BTS"/>
@@ -53,6 +30,34 @@
 {{-- <meta name="isDFP" content="true"/>
 <meta name="dfpPageType" content="nota"/>
 <meta name="dfp_path" content="infobae/inhouse"/><meta property="article:publisher" content="https://www.facebook.com/infobae"/> --}}
+
+
+	{{-- <title>your keyword rich title of the website and/or webpage</title> --}}
+	<meta property="og:image" content="https://vidriera.online/storage/{{$negocio->logo}}" />
+	<meta property="og:image:secure_url" content="https://vidriera.online/storage/{{$negocio->logo}}" />
+	<!-- MS, fb & Whatsapp -->
+	<!-- MS Tile - for Microsoft apps-->
+	<meta name="msapplication-TileImage" content="https://vidriera.online/storage/{{$negocio->logo}}">
+	<!-- fb & Whatsapp -->
+	<!-- Site Name, Title, and Description to be displayed -->
+	<meta property="og:site_name" content="{{ $negocio->nombre }}">
+	<meta property="og:title" content="{{ $negocio->nombre }}">
+	<meta property="og:description" content="{{ $negocio->descripcion }}">
+	<!-- Image to display -->
+	<meta property="og:image" content="https://vidriera.online/storage/{{$negocio->logo}}">
+	<!-- No need to change anything here -->
+	<meta property="og:type" content="website" />
+	<meta property="og:image:type" content="image/jpeg">
+	<!-- Size of image. Any size up to 300. Anything above 300px will not work in WhatsApp -->
+	<meta property="og:image:width" content="300">
+	<meta property="og:image:height" content="300">
+	<!-- Website to visit when clicked in fb or WhatsApp-->
+	<meta property="og:url" content="https://vidriera.online/{{ $negocio->url }}">
+
+
+
+
+
 
 
 
