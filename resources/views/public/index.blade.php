385168a1c09ad30620871bd550691fc3166342cb
Gómez Roque <32652492+groque0720@users.noreply.github.com>
2020-09-28 19:03:19 -0300

    Update index.blade.php

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -5,31 +5,37 @@
 @section('metas');
 
 
-{{-- <meta property="keywords" content="recitales por streaming,+Vivo,recitales,streaming,BTS"/>
-<meta name="description" content="{{ $negocio->descripcion }}"/>
-<meta name="robots" content="index, follow"/>
-<meta name="distribution" content="global"/>
-<meta name="rating" content="general"/>
-<meta name="language" content="es_ES"/>
-<meta name="news_keywords" content=""/> --}}
-{{-- <meta name="twitter:title" content="{{ $negocio->negocio }}"/>
-<meta name="twitter:site" content="@infobae"/>
-<meta name="twitter:card" content="summary_large_image"/>
-<meta name="twitter:creator" content="@infobae"/>
-<meta name="twitter:image" content="https://vidriera.online/storage/{{ $negocio->logo }}"/>
-<meta name="twitter:description" content="https://vidriera.online/storage/{{ $negocio->logo }}"/> --}}
 
-<meta property="og:title" content="Recitales por streaming: cómo se adapta la industria musical ante la “nueva normalidad”"/>
-<meta property="og:description" content="{{ $negocio->descripcion }}"/>
-<meta property="og:url" content="https://vidriera.online/{{ $negocio->url }}"/>
-<meta property="og:type" content="article"/>
-<meta property="og:site_name" content="{{ $negocio->negocio }}"/>
-<meta property="og:locale" content="es_LA"/>
-<meta itemProp="image" property="og:image" content="/storage/{{ $negocio->logo }}"/>
-<meta property="og:image:secure_url" content="/storage/{{ $negocio->logo }}" />
-{{-- <meta name="isDFP" content="true"/>
-<meta name="dfpPageType" content="nota"/>
-<meta name="dfp_path" content="infobae/inhouse"/><meta property="article:publisher" content="https://www.facebook.com/infobae"/> --}}
+
+    <meta property="og:locale" content="es_ES">
+    <meta property="og:type" content="article">
+    <meta property="og:title" content="12 Leyes de la experiencia de usuario UX - Eniun">
+    <meta property="og:description" content="Las 12 leyes más importantes de la experiencia de usuario UX. Las reglas que nos sirven de guía para generar percepciones positivas en las personas cuando interactúen con nuestros productos. Eniun, expertos en experiencia de usuario y usabilidad web.">
+    <meta property="og:url" content="https://www.eniun.com/12-leyes-experiencia-de-usuario-ux/">
+    <meta property="og:site_name" content="Eniun">
+    <meta property="article:publisher" content="https://www.facebook.com/eniun.es">
+    <meta property="article:section" content="UX">
+    <meta property="article:published_time" content="2018-02-28T09:00:04+00:00">
+    <meta property="article:modified_time" content="2019-08-22T19:41:17+00:00">
+    <meta property="og:updated_time" content="2019-08-22T19:41:17+00:00">
+    <meta property="og:image" content="https://www.eniun.com/wp-content/uploads/12-leyes-de-la-experiencia-de-usuario-UX.jpg">
+    <meta property="og:image:secure_url" content="https://www.eniun.com/wp-content/uploads/12-leyes-de-la-experiencia-de-usuario-UX.jpg">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="600">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 	{{-- <title>your keyword rich title of the website and/or webpage</title> --}}
@@ -53,18 +59,6 @@
 	<meta property="og:image:height" content="300">
 	<!-- Website to visit when clicked in fb or WhatsApp-->
 	<meta property="og:url" content="https://vidriera.online/{{ $negocio->url }}">
-
-
-
-
-
-
-
-
-
-
-
-
 
 @endsection
 
