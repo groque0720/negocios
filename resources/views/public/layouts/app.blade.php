64f6ce871703b85940f89f721a804cebd45b0b92
Gómez Roque <32652492+groque0720@users.noreply.github.com>
2020-09-28 01:12:26 -0300

    R280920200112

--- resources/views/public/layouts/app.blade.php
+++ resources/views/public/layouts/app.blade.php
@@ -12,6 +12,17 @@
     <title>@yield('title', config('app.name', 'Laravel'))</title>
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<title>your keyword rich title of the website and/or webpage</title>
+<meta name="description" content="description of your website/webpage, make sure you use keywords!">
+<meta property="og:title" content="short title of your website/webpage" />
+<meta property="og:url" content="https://www.vidriera.online/ideasenlaser/" />
+<meta property="og:description" content="description of your website/webpage">
+<meta property="og:image" content="https://www.vidriera.online/images/pp.jpg">
+<meta property="og:type" content="article" />
+<meta property="og:locale" content="en_GB" />
+<meta property="og:locale:alternate" content="fr_FR" />
+<meta property="og:locale:alternate" content="es_ES" />
+
 
 </head>
 <body class="scrollbar" id="style-3">
