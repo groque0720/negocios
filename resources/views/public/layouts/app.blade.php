71fb71a9c916655323606ccb71d47cab3d5f6a27
Gómez Roque <32652492+groque0720@users.noreply.github.com>
2020-09-28 01:29:03 -0300

    Update app.blade.php

--- resources/views/public/layouts/app.blade.php
+++ resources/views/public/layouts/app.blade.php
@@ -13,16 +13,44 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 <title>your keyword rich title of the website and/or webpage</title>
-<meta name="description" content="description of your website/webpage, make sure you use keywords!">
+{{-- <meta name="description" content="description of your website/webpage, make sure you use keywords!">
 <meta property="og:title" content="short title of your website/webpage" />
 <meta property="og:url" content="https://vidriera.online/ideasenlaser/" />
+<meta property="og:type" content="website" />
 <meta property="og:description" content="description of your website/webpage">
-<meta property="og:image" content="https://vidriera.online/images/pp.jpg">
+<meta property="og:image:secure_url" content="https://vidriera.online/images/pp.jpg">
 <meta property="og:image" itemprop="image" content="https://vidriera.online/images/pp.jpg">
 <meta property="og:type" content="article" />
 <meta property="og:locale" content="en_GB" />
 <meta property="og:locale:alternate" content="fr_FR" />
-<meta property="og:locale:alternate" content="es_ES" />
+<meta property="og:locale:alternate" content="es_ES" /> --}}
+
+<!-- MS, fb & Whatsapp -->
+
+<!-- MS Tile - for Microsoft apps-->
+<meta name="msapplication-TileImage" content="https://vidriera.online/images/pp.jpg">
+
+<!-- fb & Whatsapp -->
+
+<!-- Site Name, Title, and Description to be displayed -->
+<meta property="og:site_name" content="The Rock Photo Studio">
+<meta property="og:title" content="The Rock Photo Studio in Florida">
+<meta property="og:description" content="The best photo studio for your events">
+
+<!-- Image to display -->
+<!-- Replace   «example.com/image01.jpg» with your own -->
+<meta property="og:image" content="https://vidriera.online/images/pp.jpg">
+
+<!-- No need to change anything here -->
+<meta property="og:type" content="website" />
+<meta property="og:image:type" content="image/jpeg">
+
+<!-- Size of image. Any size up to 300. Anything above 300px will not work in WhatsApp -->
+<meta property="og:image:width" content="300">
+<meta property="og:image:height" content="300">
+
+<!-- Website to visit when clicked in fb or WhatsApp-->
+<meta property="og:url" content="https://vidriera.online/ideasenlaser">
 
 
 </head>
