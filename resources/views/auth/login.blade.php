12ea5443742e477567451986b88eefebca6b309e
Gómez Roque <32652492+groque0720@users.noreply.github.com>
2021-01-21 11:05:38 -0300

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,14 +10,14 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     {{-- <title>@yield('title', config('app.name', 'Login'))</title> --}}
-    <title>Tu Vidriera en Internet</title>
-<meta property="og:locale" content="es_ES" />
-<meta property="og:type" content="website" />
-<meta property="og:title" content="Tu Negocio en Internet a vista de todos" />
-<meta property="og:description" content="Fácil, rápido y sencillo de mostrar tus productos" />
-<meta property="og:url" content="https://vidriera.online/" />
-<meta property="og:site_name" content="Alcanza más clientes | Aumenta tus ventas" />
-    <!-- Para facebook, SMS -->
+    <title>Tus Productos en Internet</title>
+    <meta property="og:locale" content="es_ES" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Tu Negocio en Internet a vista de todos" />
+    <meta property="og:description" content="Fácil, rápido y sencillo de mostrar tus productos" />
+    <meta property="og:url" content="https://vidriera.online/" />
+    <meta property="og:site_name" content="Alcanza más clientes | Aumenta tus ventas" />
+        <!-- Para facebook, SMS -->
     <meta property="og:image" itemprop="image" content="https://vidriera.online/logo_redes_sociales.png"/>
     <meta property="og:image:secure_url" content="https://vidriera.online/logo_redes_sociales.png" />
     <meta property="og:image:width" content="1087" />
